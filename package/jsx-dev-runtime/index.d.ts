634511cfaedee284eeee9410a8e09c3b7cbd8c65
paglobal <jackamoah.pa@gmail.com>
2024-06-14 15:50:51 +0000

    gitignore jsx-dev-runtime folder

--- package/jsx-dev-runtime/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { jsx, jsx as jsxs, jsx as jsxDEV, JSX, Fragment } from "../jsx-runtime";
-export { jsx, jsxs, jsxDEV, JSX, Fragment };
-//# sourceMappingURL=index.d.ts.map
--- package/jsx-dev-runtime/index.js
+++
@@ -1,2 +0,0 @@
-import { jsx, jsx as jsxs, jsx as jsxDEV, Fragment } from "../jsx-runtime";
-export { jsx, jsxs, jsxDEV, Fragment };
